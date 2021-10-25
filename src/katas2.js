a01246d88100f70f8fb28b3462ac98c2d775cda9
moisesmacedo <moises.macedo@hotmail.com.br>
2021-10-25 20:19:08 -0300

    Update katas2.js

--- src/katas2.js
+++ src/katas2.js
@@ -1,7 +1,10 @@
 // comece a criar a sua função add na linha abaixo
 
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+
+function add() {}
+
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado')
 
 
 // comece a criar a sua função multiply na linha abaixo
