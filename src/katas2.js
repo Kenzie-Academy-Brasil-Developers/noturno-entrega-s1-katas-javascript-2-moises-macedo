65e299612172dace77d0cc93c264a3ae5e290ac3
Moisés Alex de Castro Macedo <moises.macedo@hotmail.com.br>
2022-02-22 16:56:43 -0300

    finalizado

--- src/katas2.js
+++ src/katas2.js
@@ -1,35 +1,45 @@
-// comece a criar a sua função add na linha abaixo
+function add(a, b){
 
-// descomente a linha seguinte para testar sua função
-
-/*function add() {
-
-console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado')
-
-}*/
-
-
-// comece a criar a sua função multiply na linha abaixo
-
-/*function multiply (){}
-
-
-// descomente a linha seguinte para testar sua função
- console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');*/
-
-
-// comece a criar a sua função power na linha abaixo
-/*function power (){}
-
-// descomente a linha seguinte para testar sua função
-console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');*/
-
-
-// comece a criar a sua função factorial na linha abaixo
-/*function factorial (){}
-
-// descomente a linha seguinte para testar sua função
-console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');*/
+    return a + b;
+  }
+  
+  console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  function multiply(a, b){
+    let multiplicacao = 0;
+  
+    for(let contador = 1; contador <= b; contador++){
+        multiplicacao = add(a, multiplicacao);
+    }
+  
+    return multiplicacao;
+  }
+  console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  function power(a, b){
+    let potencia = a;
+  
+    for( let contador = 1; contador < b; contador++){
+      potencia = multiply(a, potencia);
+    }
+  
+    return potencia;
+  }
+  console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  function factorial(a){
+    let fatorial = 1;
+  
+    for(let contador = 1; contador <= a; contador++){
+        fatorial = multiply(fatorial,contador);
+    }
+  
+    return fatorial;
+  }
+  console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
